8007eece4fcb943fe821bc350d3dbdea2c90edb3
Peter Lesslie <pclesslie@gmail.com>
2015-12-26 11:22:42 -0600

    Print man based on number of misses

--- main.py
+++ main.py
@@ -12,9 +12,68 @@ def get_guess():
         raise Exception("Letter entered not valid: " + letter)
     return letter
 
+
 def generate_word():
     words = ["hello", "goodbye", "mystery"]
     return random.choice(words)
+
+
+def print_man(misses):
+    if misses == 0:
+        print """
+    =====
+    |   |
+    |
+    |
+    |
+    |
+---------"""
+    elif misses == 1:
+        print """
+    =====
+    |   |
+    |   0
+    |
+    |
+    |
+---------"""
+    elif misses == 2:
+        print """
+    =====
+    |   |
+    |   0
+    | /[ ]\\
+    |
+    |
+---------"""
+    elif misses == 3:
+        print """
+    =====
+    |   |
+    |   0
+    | /[ ]\\
+    |  [ ]
+    |
+---------"""
+    elif misses == 4:
+        print """
+    =====
+    |   |
+    |   0
+    | /[ ]\\
+    |  [ ]
+    |  /
+---------"""
+    else:
+        print """
+    =====
+    |   |
+    |   0
+    | /[ ]\\
+    |  [ ]
+    |  / \\
+---------"""
+
 
 def main():
     word = generate_word()
@@ -22,12 +81,14 @@ def main():
     letters = []
     guesses = 0
     misses = 0
-
-    print "The word is: ", word
+    #print "The word is: ", word
     while misses < 5:
+        print_man(misses)
+        print "Guessed letters: ", ','.join(letters), "\n"
         print ' '.join(state)
         guess = get_guess()
         print "guess was: ", guess
+        letters.append(guess)
         guesses += 1
         guessed_correct = False
         for i, letter in enumerate(word):
@@ -38,9 +99,14 @@ def main():
             print "Miss!"
             misses += 1
         elif ''.join(state) == word:
-            print ' '.join(state)
-            print "You won!"
             break
+    print_man(misses)
+    print ' '.join(state)
+    if misses < 5:
+        print "You won!"
+    else:
+        print "You lost!"
+        print "The word was: ", word
 
 if __name__ == '__main__':
     main()
